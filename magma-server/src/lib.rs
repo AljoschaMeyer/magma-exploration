79f6b22ab5e8c6fdfb906f5a0fbfb80401918cbc
memoryleak47 <memoryleak47@protonmail.ch>
2021-03-09 15:30:38 +0100

    added Option<_> to Storage trait

--- magma-server/src/lib.rs
+++ magma-server/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(never_type)]
+
 use magma_common::*;
 
 use std::collections::HashMap;
@@ -8,31 +10,31 @@ use async_trait::async_trait;
 use futures::Stream;
 
 #[async_trait(?Send)]
-pub trait Storage<G: GenericHasher, T> {
-	async fn get<'s>(&'s self, id: &Id<G>) -> Option<&'s T>;
-	async fn get_entry(&self, id: &Id<G>) -> Entry<G>;
+pub trait Storage<G: GenericHasher, T, E> {
+	async fn get<'s>(&'s self, id: &Id<G>) -> Result<Option<&'s T>, E>;
+	async fn get_entry(&self, id: &Id<G>) -> Result<Option<Entry<G>>, E>;
 }
 
-pub struct Server<G: GenericHasher, T, S: Storage<G, T>> {
+pub struct Server<G: GenericHasher, T, E, S: Storage<G, T, E>> {
 	storage: S,
-	_phantom: PhantomData<(G, T)>,
+	_phantom: PhantomData<(G, T, E)>,
 }
 
-impl<G: GenericHasher, T, S: Storage<G, T>> Server<G, T, S> {
+impl<G: GenericHasher, T, E, S: Storage<G, T, E>> Server<G, T, E, S> {
 	fn reply(request: Request<G>) -> impl Stream<Item=T> {
-		// TODO
+		futures::stream::empty() // TODO
 	}
 }
 
 struct InMemory<G: GenericHasher, T>(HashMap<Id<G>, T>);
 
 #[async_trait(?Send)]
-impl<G: GenericHasher, T> Storage<G, T> for InMemory<G, T> where G::Digest: Hash + Eq {
-	async fn get<'s>(&'s self, id: &Id<G>) -> Option<&'s T> {
-		self.0.get(id)
+impl<G: GenericHasher, T> Storage<G, T, !> for InMemory<G, T> where G::Digest: Hash + Eq {
+	async fn get<'s>(&'s self, id: &Id<G>) -> Result<Option<&'s T>, !> {
+		Ok(self.0.get(id))
 	}
 
-	async fn get_entry(&self, id: &Id<G>) -> Entry<G> {
+	async fn get_entry(&self, id: &Id<G>) -> Result<Option<Entry<G>>, !> {
 		unimplemented!()
 	}
 }
