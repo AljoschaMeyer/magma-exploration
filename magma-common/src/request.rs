37190110ee4ac745204216421f683d637919b064
memoryleak47 <memoryleak47@protonmail.ch>
2021-03-09 15:39:58 +0100

    added granularities

--- magma-common/src/request.rs
+++ magma-common/src/request.rs
@@ -3,4 +3,12 @@ use crate::*;
 pub struct Request<G: GenericHasher> {
 	id: Id<G>,
 	origin: Option<Id<G>>,
+	min_gran: Granularity,
+	max_gran: Granularity,
+}
+
+enum Granularity {
+	Direct, // give me only the payload of `id` -- 1 payload
+	SkipLinks, // -- O(log n) payloadpayloads
+	PredLinks, // -- O(n) payloads
 }
