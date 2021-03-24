1fd2f075a941487d8dac9bb497745f23aaa9da3c
memoryleak47 <memoryleak47@protonmail.ch>
2021-03-24 15:17:58 +0100

    ~request

--- magma-common/src/request.rs
+++ magma-common/src/request.rs
@@ -1,10 +1,8 @@
 use crate::*;
 
-pub struct Request<G: GenericHasher> {
-	id: Id<G>,
-	origin: Option<Id<G>>,
-	min_gran: Granularity,
-	max_gran: Granularity,
+enum Offset {
+	Entries(/*sequence_num:*/ u64), // only send the entries with sequence number <= sequence_num
+	BytesWithinPayload(u64),
 }
 
 enum Granularity {
@@ -12,4 +10,12 @@ enum Granularity {
 	Direct, // give me only the payload of `id` -- 1 payload
 	SkipLinks, // -- O(log n) payloads
 	PredLinks, // -- O(n) payloads
+}
+
+pub struct Request<G: GenericHasher> {
+	id: Id<G>,
+	origin: Option<Id<G>>,
+	min_gran: Granularity,
+	max_gran: Granularity,
+	offset: Offset,
 }
